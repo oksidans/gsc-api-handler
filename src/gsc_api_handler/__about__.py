11ddbef071ca0febc13e8482e88d1931ab126ac0
Miroljub Zivkovic <zmiroljub.zivkovic@gmail.com>
2025-12-06 23:05:14 +0100

    Release 1.0.3: fix public API and metadata

--- src/gsc_api_handler/__about__.py
+++ src/gsc_api_handler/__about__.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2025-present bcopauto <mzivkovic@bettercollective.com>
+# SPDX-FileCopyrightText: 2025-present oksidans <zmiroljub.zivkovic@gmail.com>
 #
 # SPDX-License-Identifier: MIT
-__version__ = "1.0.2"
+__version__ = "1.0.3"
--- src/gsc_api_handler/__init__.py
+++ src/gsc_api_handler/__init__.py
@@ -1,1 +1,4 @@
-from .auth import authorize_creds
+from .fetcher import fetch_and_store_gsc_data
+
+
+__all__ = ["authorize_creds", "fetch_and_store_gsc_data"]
